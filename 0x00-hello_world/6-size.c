137b93c9b75d05ca469dd7416b2ca428b68a4ef7
attahiermahmud <attaheermamud@gmail.com>
2022-10-17 17:30:25 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,5 @@ int main(void)
 	printf("Size of c long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of d long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of f float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
