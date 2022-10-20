7bab1bcff4660617812ae29894e5994e0435db2b
attahiermahmud <attaheermamud@gmail.com>
2022-10-20 15:40:20 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Writes the char c to stdout
+ * @c: The character to print
+ * Return: On succes if 1 else o
+ */
+int _isupper(int c)
+{
+	if (c >= "A" && c <= "Z")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
