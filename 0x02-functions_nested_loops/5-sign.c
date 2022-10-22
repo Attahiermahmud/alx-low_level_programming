89e02d9c68809a83a195949d06e5c0ae1c20735f
attahiermahmud <attaheermamud@gmail.com>
2022-10-21 17:15:43 -0700

    Triangle

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_sign - Entr point
+ *
+ * @n: carrier variDescription: prints the sign of a number
+ *
+ * Description: prints the sign of a number
+ */
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+
+	{
+
+		_putchar('+');
+
+		return (1);
+
+	}
+
+	else if (n < 0)
+
+	{
+
+		_putchar('-');
+
+		return (-1);
+
+	}
+
+	_putchar('0');
+
+	return (0);
+
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int  year);
 void int convert_day(int month, int day);
+
 
 #endif
